761ed4abb904844456a58c0ea23acc4cc38bfda3
Tejas013 <tejasmadhukar6@gmail.com>
2023-11-02 02:22:35 +0530

    Added file reading and json unmarshalling

--- main.go
+++ main.go
@@ -1,25 +1,50 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 )
 
 const (
-  input_dir = "./data"
-  output_dir = "./output"
+	input_dir  = "./data"
+	output_dir = "./output"
 )
 
 func main() {
-  fmt.Println("Reading files from ",input_dir)
+	var data []map[string]interface{}
 
-  files, err := os.ReadDir(input_dir)
-  if err != nil {
-    log.Fatal(err)
-  }
-  
-  for _,el := range files {
-    fmt.Println(el)
-  }
+	fmt.Println("Reading files from ", input_dir)
+
+	files, err := os.ReadDir(input_dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		file_path := input_dir + "/" + file.Name()
+
+		f, err := os.Open(file_path)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = json.NewDecoder(f).Decode(&data)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, obj := range data {
+			if obj == nil {
+				continue
+			}
+
+			for key, value := range obj {
+				fmt.Println(key, value)
+			}
+		}
+
+		f.Close()
+	}
 }
